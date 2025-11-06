caf74887ef1b022783056d79737dab9acc825304
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-11-06 12:11:55 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,7 +2,7 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+ansible-pull -U https://github.com/nediminenipavan-tech/ansible-roboshop-roles.git -e component=$component main.yaml
 # git clone ansible-playbook
 # cd ansible-playbook
 # ansible-playbook -i inventory main.yaml
