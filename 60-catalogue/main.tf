cdbf37ff22d35d0a8fe860b7f85b8013c3bdc3f5
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-11-08 18:28:03 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -40,8 +40,9 @@ resource "terraform_data" "catalogue" {
     ]
   }
 }
+  
 
-# stop the instance to take image
+  # stop the instance to take image
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
   state       = "stopped"
@@ -58,67 +59,4 @@ resource "aws_ami_from_instance" "catalogue" {
             Name = "${local.common_name_suffix}-catalogue-ami" # roboshop-dev-mongodb
         }
   )
-}
-
-resource "aws_lb_target_group" "catalogue" {
-  name     = "${local.common_name_suffix}-catalogue"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
-  deregistration_delay = 60 # waiting period before deleting the instance
-
-  health_check {
-    healthy_threshold = 2
-    interval = 10
-    matcher = "200-299"
-    path = "/health"
-    port = 8080
-    protocol = "HTTP"
-    timeout = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_launch_template" "catalogue" {
-  name = "${local.common_name_suffix}-catalogue"
-  image_id = aws_ami_from_instance.catalogue.id
-
-  instance_initiated_shutdown_behavior = "terminate"
-  instance_type = "t3.micro"
-
-  vpc_security_group_ids = [local.catalogue_sg_id]
-
-  # tags attached to the instance
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = merge(
-      local.common_tags,
-      {
-        Name = "${local.common_name_suffix}-catalogue"
-      }
-    )
-  }
-
-  # tags attached to the volume created by instance
-  tag_specifications {
-    resource_type = "volume"
-
-    tags = merge(
-      local.common_tags,
-      {
-        Name = "${local.common_name_suffix}-catalogue"
-      }
-    )
-  }
-
-  # tags attached to the launch template
-  tags = merge(
-      local.common_tags,
-      {
-        Name = "${local.common_name_suffix}-catalogue"
-      }
-  )
-
-}
-
+} 
