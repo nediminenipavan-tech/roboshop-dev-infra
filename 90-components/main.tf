f0cb84af411690a255282467cacfe1f582db7824
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-11-11 21:28:00 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,8 +1,8 @@
- module "components" {
+ /* module "components" {
     source = "../../terraform-roboshop-component"
     component = var.component
     rule_priority = var.rule_priority
-}
+} */
  
 module "components" {
     for_each = var.components
