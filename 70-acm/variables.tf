f61bf46e4dd21d40f90218f1776c745af7439b81
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-11-11 12:14:28 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+variable "zone_id" {
+    default = "Z00155401S0S0TANIYY1H"
+}
+
+variable "domain_name" {
+    default = "daws86pavan.cyou"
+}
+ 
